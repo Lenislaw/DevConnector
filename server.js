570adf7cbd2024cced3d2f10f3819e66540f560d
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-06-29 18:42:52 +0200

    Connect MangoDB

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
+// Express init
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API Running");
